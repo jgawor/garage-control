44c6e9cae7f068122b519e6a42ab017a2e1ef5ef
Jarek Gawor <jgawor@gmail.com>
2016-05-08 20:51:40 -0400

    minor fix

--- pi/scripts/update_status.sh
+++ pi/scripts/update_status.sh
@@ -4,6 +4,6 @@ CONFIG="./conf/config.json"
 
 status=`./scripts/control.sh status`
 user=`jq -r .id $CONFIG`
-password=`jq -r .door_password $CONFIG`
+password=`jq -r .register_password $CONFIG`
 
 curl --retry 3 -H "Content-Type: application/json" -X POST -d "{\"status\":$status}" --user $user:$password https://mygarage.mybluemix.net/status
